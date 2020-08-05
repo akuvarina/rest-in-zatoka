8cf10cb401a5dc8b9d7bbcfc90be666e1f061184
Kuvarina, Anna <anna.kuvarina@sigma.software>
2020-08-05 22:55:31 +0300

    created script, moved files

---
+++ js/script.js
@@ -0,0 +1,84 @@
+// home section
+const pageTitle = document.getElementById('title');
+pageTitle.innerText = config.title;
+const phones = document.querySelectorAll('.phone-number');
+
+for (let i = 0; i < phones.length; i++) {
+    phones[i].innerText = config.phoneNumber;
+}
+
+// about section
+const aboutText = document.getElementById('about-text');
+aboutText.innerText = config.aboutText;
+
+// rooms section
+const roomsWrapper = document.getElementById('rooms-wrapper');
+config.rooms.forEach((room, index) => {
+    const { title, description, list } = room;
+    const slideNumber = index + 1;
+
+    list.map((item) => (
+        `<li class="list-item"><img src="./img/list-arrow.png" class="list-arrow-icon" alt="list-arrow">${item}</li>`
+    ));
+
+    const items = list.join('');
+
+    const slideContent = `
+    <div class="carousel-item">
+                    <div class="rooms-row">
+                        <article class="room-info">
+                            <h3 class="room-title">
+                                ${title}
+                            </h3>
+                            <p class="description">
+                                ${description}
+                            </p>
+                            <ul>${items}</ul>
+                        </article>
+                        <div class="room-img room-img-50 slide${slideNumber} photo-1"></div>
+                    </div>
+                    <div class="rooms-row">
+                        <div class="room-img room-img-30 slide${slideNumber} photo-2"></div>
+                        <div class="room-img room-img-30 slide${slideNumber} photo-3"></div>
+                        <div class="room-img room-img-30 slide${slideNumber} photo-4"></div>
+                    </div>
+                </div>
+    `;
+
+    const slideEl = document.createElement('div');
+    slideEl.classList.add('carousel-item');
+
+    if (index === 0) {
+        slideEl.classList.add('active');
+    }
+
+    slideEl.innerHTML = slideContent;
+    roomsWrapper.appendChild(slideEl);
+});
+
+// comments section
+const commentsWrapper = document.getElementById('comments-wrapper');
+config.comments.forEach((comment, index) => {
+    const { name, profession, date, comment: commentText } = comment;
+    const articleEl = document.createElement('article');
+    const commentNumber = index + 1;
+    const commentContent = `
+    <div class="card-title-row">
+                    <img src="./img/comments/comment${commentNumber}.png" alt="comment" class="card-img">
+                    <div class="commentator-container">
+                        <span class="commentator-name">${name}</span>
+                        <span class="commentator-info">${profession}</span>
+                        <span class="commentator-date">${date}</span>
+                    </div>
+                </div>
+
+                <p class="comment">${commentText}</p>
+               
+    `;
+    articleEl.innerHTML = commentContent;
+    commentsWrapper.appendChild(articleEl);
+});
+
+// contacts section
+const addressEl = document.getElementById('address');
+addressEl.innerText = config.contacts.address;
