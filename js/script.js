f1de4505ae2eea0579973ff9259d7d6bcc1d5c10
Kuvarina, Anna <anna.kuvarina@sigma.software>
2020-08-05 23:13:56 +0300

    used scripts

--- js/script.js
+++ js/script.js
@@ -17,32 +17,25 @@ config.rooms.forEach((room, index) => {
     const { title, description, list } = room;
     const slideNumber = index + 1;
 
-    list.map((item) => (
-        `<li class="list-item"><img src="./img/list-arrow.png" class="list-arrow-icon" alt="list-arrow">${item}</li>`
-    ));
-
-    const items = list.join('');
+    let items = '';
+    list.forEach((item) => {
+        items += `<li class="list-item"><img src="./img/list-arrow.png" class="list-arrow-icon" alt="list-arrow">${item}</li>`;
+    });
 
     const slideContent = `
-    <div class="carousel-item">
-                    <div class="rooms-row">
-                        <article class="room-info">
-                            <h3 class="room-title">
-                                ${title}
-                            </h3>
-                            <p class="description">
-                                ${description}
-                            </p>
-                            <ul>${items}</ul>
-                        </article>
-                        <div class="room-img room-img-50 slide${slideNumber} photo-1"></div>
-                    </div>
-                    <div class="rooms-row">
-                        <div class="room-img room-img-30 slide${slideNumber} photo-2"></div>
-                        <div class="room-img room-img-30 slide${slideNumber} photo-3"></div>
-                        <div class="room-img room-img-30 slide${slideNumber} photo-4"></div>
-                    </div>
-                </div>
+    <div class="rooms-row">
+    <article class="room-info">
+    <h3 class="room-title">${title}</h3>
+    <p class="description">${description}</p>
+    <ul>${items}</ul>
+    </article>
+    <div class="room-img room-img-50 slide${slideNumber} photo-1"></div>
+    </div>
+    <div class="rooms-row">
+    <div class="room-img room-img-30 slide${slideNumber} photo-2"></div>
+    <div class="room-img room-img-30 slide${slideNumber} photo-3"></div>
+    <div class="room-img room-img-30 slide${slideNumber} photo-4"></div>
+    </div>
     `;
 
     const slideEl = document.createElement('div');
@@ -61,19 +54,18 @@ const commentsWrapper = document.getElementById('comments-wrapper');
 config.comments.forEach((comment, index) => {
     const { name, profession, date, comment: commentText } = comment;
     const articleEl = document.createElement('article');
+    articleEl.classList.add('comments-card');
     const commentNumber = index + 1;
     const commentContent = `
     <div class="card-title-row">
-                    <img src="./img/comments/comment${commentNumber}.png" alt="comment" class="card-img">
-                    <div class="commentator-container">
-                        <span class="commentator-name">${name}</span>
-                        <span class="commentator-info">${profession}</span>
-                        <span class="commentator-date">${date}</span>
-                    </div>
-                </div>
-
-                <p class="comment">${commentText}</p>
-               
+    <img src="./img/comments/comment${commentNumber}.png" alt="comment" class="card-img">
+    <div class="commentator-container">
+    <span class="commentator-name">${name}</span>
+    <span class="commentator-info">${profession}</span>
+    <span class="commentator-date">${date}</span>
+    </div>
+    </div>
+    <p class="comment">${commentText}</p>          
     `;
     articleEl.innerHTML = commentContent;
     commentsWrapper.appendChild(articleEl);
